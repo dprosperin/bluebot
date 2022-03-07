d7f10f04f3aa0d27f042b53fe74cf5ccf832fea7
David Prospérin <32713756+dprosperin@users.noreply.github.com>
2022-03-07 23:05:07 +0100

    renommer les constantes MOTEUR

--- config.h
+++ config.h
@@ -2,7 +2,13 @@
 #define BAUDRATE 9600
 #define RXPIN 11  // Vers la broche TX du module bluetooth
 #define TXPIN 12  // Vers la broche RX du module bluetooth 
-// note : la broche RX du module bluetooth n'est pas branché au microcontrôleur
+/*
+ * NOTE :
+ * La broche RX du module bluetooth n'est pas branché au microcontrôleur.
+ * Donc les messages provenant du module bluetooth ne seront pas affichés.
+ * 
+ */
+
 
 #define TIMEOUT 5
 #define TERMINATOR '\n'
@@ -10,10 +16,11 @@
 // 9 et 6 Moteur Droite
 // 3 et 5 Moteur Gauche
 
-#define MOTEUR_GAUCHE_1 3
-#define MOTEUR_GAUCHE_2 5
+#define MOTEUR_GAUCHE_FORWARD 3
+#define MOTEUR_GAUCHE_BACKWARD 5
 
-#define MOTEUR_DROIT_1 6
-#define MOTEUR_DROIT_2 9
+#define MOTEUR_DROIT_FORWARD 9
+#define MOTEUR_DROIT_BACKWARD 6
+
 
 #define MOVING_TIME 1000 // Le temps en milliseconde pendant lequel on active le moteur
--- deplacement.ino
+++ deplacement.ino
@@ -1,31 +1,31 @@
 void avancer(unsigned long temps) {
-  analogWrite(MOTEUR_GAUCHE_2, 0);
-  analogWrite(MOTEUR_GAUCHE_1, 200);
-
-  analogWrite(MOTEUR_DROIT_1, 0);
-  analogWrite(MOTEUR_DROIT_2, 200);
+  analogWrite(MOTEUR_GAUCHE_FORWARD, 200);
+  analogWrite(MOTEUR_GAUCHE_BACKWARD, 0);
+  
+  analogWrite(MOTEUR_DROIT_FORWARD, 200);
+  analogWrite(MOTEUR_DROIT_BACKWARD, 0);
 
   delay(temps);
 
-  analogWrite(MOTEUR_GAUCHE_2, 0);
-  analogWrite(MOTEUR_GAUCHE_1, 0);
+  analogWrite(MOTEUR_GAUCHE_BACKWARD, 0);
+  analogWrite(MOTEUR_GAUCHE_FORWARD, 0);
 
-  analogWrite(MOTEUR_DROIT_1, 0);
-  analogWrite(MOTEUR_DROIT_2, 0);
+  analogWrite(MOTEUR_DROIT_BACKWARD, 0);
+  analogWrite(MOTEUR_DROIT_FORWARD, 0);
 }
 
 void reculer(unsigned long temps){
-  analogWrite(MOTEUR_GAUCHE_2, 200);
-  analogWrite(MOTEUR_GAUCHE_1, 0);
+  analogWrite(MOTEUR_GAUCHE_BACKWARD, 200);
+  analogWrite(MOTEUR_GAUCHE_FORWARD, 0);
 
-  analogWrite(MOTEUR_DROIT_1, 200);
-  analogWrite(MOTEUR_DROIT_2, 0);
+  analogWrite(MOTEUR_DROIT_BACKWARD, 200);
+  analogWrite(MOTEUR_DROIT_FORWARD, 0);
 
   delay(temps);
 
-  analogWrite(MOTEUR_GAUCHE_2, 0);
-  analogWrite(MOTEUR_GAUCHE_1, 0);
+  analogWrite(MOTEUR_GAUCHE_BACKWARD, 0);
+  analogWrite(MOTEUR_GAUCHE_FORWARD, 0);
 
-  analogWrite(MOTEUR_DROIT_1, 0);
-  analogWrite(MOTEUR_DROIT_2, 0);
+  analogWrite(MOTEUR_DROIT_BACKWARD, 0);
+  analogWrite(MOTEUR_DROIT_FORWARD, 0);
 }
