7af9d60017ae91ff104fbee00473ea58ae9460e9
David PROSPERIN <david.prosperin@lasallesaintdenis.com>
2022-03-07 17:00:03 +0100

    déplacement pendant un certain temps

--- bluetooth_robot_HC-06.ino
+++ bluetooth_robot_HC-06.ino
@@ -1,29 +1,45 @@
 #include "config.h"
 
-void reculer();
-void avancer();
+void reculer(unsigned long temps);
+void avancer(unsigned long temps);
 void setup() {
     Serial.print("Sketch: "); Serial.println(__FILE__);
     Serial.print("Uploaded: "); Serial.println(__DATE__);
 }
 
 void loop() {
-  reculer();
+  reculer(1000);
   delay(1000);
 }
 
-void avancer() {
+void avancer(unsigned long temps) {
   analogWrite(MOTEUR_GAUCHE_2, 0);
   analogWrite(MOTEUR_GAUCHE_1, 200);
 
   analogWrite(MOTEUR_DROIT_1, 0);
   analogWrite(MOTEUR_DROIT_2, 200);
+
+  delay(temps);
+
+  analogWrite(MOTEUR_GAUCHE_2, 0);
+  analogWrite(MOTEUR_GAUCHE_1, 0);
+
+  analogWrite(MOTEUR_DROIT_1, 0);
+  analogWrite(MOTEUR_DROIT_2, 0);
 }
 
-void reculer(){
+void reculer(unsigned long temps){
   analogWrite(MOTEUR_GAUCHE_2, 200);
   analogWrite(MOTEUR_GAUCHE_1, 0);
 
   analogWrite(MOTEUR_DROIT_1, 200);
+  analogWrite(MOTEUR_DROIT_2, 0);
+
+  delay(temps);
+
+  analogWrite(MOTEUR_GAUCHE_2, 0);
+  analogWrite(MOTEUR_GAUCHE_1, 0);
+
+  analogWrite(MOTEUR_DROIT_1, 0);
   analogWrite(MOTEUR_DROIT_2, 0);
 }
